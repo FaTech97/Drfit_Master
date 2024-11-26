b8dcf4f06b4435e9e5c3adb8b372d7455acd04d9
Dmitry Litvinov <DmLtvnv@yandex.ru>
2024-11-26 22:02:37 +0300

    feat: transalte - add german, french and spain langs

--- Assets/_GAME/scripts/LanguageWindowController.cs
+++ Assets/_GAME/scripts/LanguageWindowController.cs
@@ -11,7 +11,10 @@ public enum Langs
 {
 	English,
 	Russian,
-	Turkish
+	Turkish,
+	German,
+	French,
+	Spanish
 }
 
 public class LanguageWindowController : MonoBehaviour
@@ -19,26 +22,40 @@ public class LanguageWindowController : MonoBehaviour
 	public event Action<Langs> OnLangChanged;
 	[SerializeField] private Button close;
 	[SerializeField] private Button ruButton;
-	[SerializeField] private Button enButton;
-	[SerializeField] private Button turkButton;
-	[SerializeField] private GameObject settings;
 	[SerializeField] private Image ruImage;
+	[SerializeField] private Button enButton;
 	[SerializeField] private Image enImage;
+	[SerializeField] private Button turkButton;
 	[SerializeField] private Image tuImage;
+	[SerializeField] private Button germButton;
+	[SerializeField] private Image germImage;
+	[SerializeField] private Button frButton;
+	[SerializeField] private Image frImage;
+	[SerializeField] private Button spButton;
+	[SerializeField] private Image spImage;
+	[SerializeField] private GameObject settings;
 
 	void Start()
 	{
 		close.onClick.AddListener(ClosePressed);
 		ruButton.onClick.AddListener(RuPressed);
 		enButton.onClick.AddListener(EnPressed);
+		germButton.onClick.AddListener(GermPressed);
+		frButton.onClick.AddListener(FrPressed);
+		spButton.onClick.AddListener(SpPressed);
 		turkButton.onClick.AddListener(TuPressed);
 	}
+
+
 
 	private void OnDestroy()
 	{
 		close.onClick.RemoveListener(ClosePressed);
 		ruButton.onClick.RemoveListener(RuPressed);
 		enButton.onClick.RemoveListener(EnPressed);
+		turkButton.onClick.RemoveListener(TuPressed);
+		frButton.onClick.RemoveListener(FrPressed);
+		spButton.onClick.RemoveListener(SpPressed);
 		turkButton.onClick.RemoveListener(TuPressed);
 	}
 
@@ -50,6 +67,9 @@ public class LanguageWindowController : MonoBehaviour
 		ChangeChoosesByLang(lang, Langs.Russian, ruImage);
 		ChangeChoosesByLang(lang, Langs.English, enImage);
 		ChangeChoosesByLang(lang, Langs.Turkish, tuImage);
+		ChangeChoosesByLang(lang, Langs.French, frImage);
+		ChangeChoosesByLang(lang, Langs.German, germImage);
+		ChangeChoosesByLang(lang, Langs.Spanish, spImage);
 	}
 
 	public void ShowLangPage()
@@ -67,5 +87,11 @@ public class LanguageWindowController : MonoBehaviour
 	private void EnPressed() => ChooseLang(Langs.English);
 	private void RuPressed() => ChooseLang(Langs.Russian);
 	private void TuPressed() => ChooseLang(Langs.Turkish);
+
+	private void SpPressed() => ChooseLang(Langs.Spanish);
+
+	private void FrPressed() => ChooseLang(Langs.French);
+
+	private void GermPressed() => ChooseLang(Langs.German);
 	private void ClosePressed() => settings.SetActive(false);
 }
